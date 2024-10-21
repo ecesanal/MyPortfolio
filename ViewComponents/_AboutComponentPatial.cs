a20b10f161542c243d16c8c947413aa4f0fe84f2
ecesanal <elifecesanal4@gmail.com>
2024-10-21 18:41:22 +0300

    Change Partial

---
+++ ViewComponents/_AboutComponentPatial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _AboutComponentPatial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewComponents/_PortfoioComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _PortfoioComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
