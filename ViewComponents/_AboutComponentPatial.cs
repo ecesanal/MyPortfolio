5079effb7276f86d4608969411eddd2c2ba825b0
ecesanal <elifecesanal4@gmail.com>
2024-10-29 14:04:03 +0300

    project views changes

--- ViewComponents/_AboutComponentPatial.cs
+++ ViewComponents/_AboutComponentPatial.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents
 {
     public class _AboutComponentPatial : ViewComponent
     {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext();
         public IViewComponentResult Invoke()
         {
+            ViewBag.aboutTitle=portfolioContext.Abouts.Select(x => x.Title).FirstOrDefault();
+            ViewBag.aboutSubDescription=portfolioContext.Abouts.Select(x=>x.SubDescription).FirstOrDefault();
+            ViewBag.aboutDetail=portfolioContext.Abouts.Select(x=>x.Detail).FirstOrDefault();
             return View();
         }
     }
