f8500a94b80fca514c7b309a2f3714dd88537df4
ecesanal <elifecesanal4@gmail.com>
2024-11-06 18:26:21 +0300

    admin panel changes

---
+++ Controllers/ExperienceController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.Controllers
+{
+	public class ExperienceController : Controller
+	{
+		MyPortfolioContext  context = new MyPortfolioContext();
+		public IActionResult ExperienceList()
+		{
+			var values = context.Experiences.ToList();
+			return View(values);
+		}
+	}
+}
---
+++ ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.ViewComponents.LayoutViewComponents._LayoutNavbarComponentPartial
+{
+	public class _LayoutNavbarComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
